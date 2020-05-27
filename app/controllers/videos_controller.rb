f1cdc452da7f0ff08e83c7153dd35c1046b1c10c
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-26 17:20:30 -0700

    Added initial index action.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all.order(:title)
 
-    # render json: { ok: 'RENDERING JSON??'}, status: :ok
+    render json: { ok: 'Rendering JSON now!}, status: :ok
   end
 
   def show
