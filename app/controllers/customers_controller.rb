08d9e9dcb36e10deda8071aa853b057947340faa
Ada <ada@Adas-MacBook-Air.local>
2020-05-26 20:34:25 -0700

    return proper fields for a list of customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
     def index 
-        @customers = Customer.all.order(:name)
+        customers = Customer.all.order(:name)
+        render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]),
+                                         status: :ok
     end 
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -6,4 +6,24 @@ describe CustomersController do
     must_respond_with :success
     expect(response.header['Content-Type']).must_include 'json'
   end 
+  it "will return the proper fields for a list of customer" do 
+    customer_field = ["id", "name", "registered_at", "address", "city", "state", "postal_code", "phone"].sort 
+    get customers_path
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array 
+    
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash
+      expect(customer.keys.sort).must_equal customer_field
+    end 
+  end
+
+  # it "returns an empty array if no customers exist" do 
+  #   Customer.destroy_all
+
+  #   get customers_path
+
+  #   body = 
+  # end 
 end
