2328485d2a09b91dc8724a95527268481eb4ad79
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-27 17:30:53 -0700

    Scaffolded tests for set_customer and set_movie.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,21 +7,52 @@ class RentalsController < ApplicationController
   # check-out instance method
 
 
+
+
+
+
+
+
+
+
+  
+
   # check-in instance method
+
+
+
+
+
+
+
+
+
+
+
 
   private
 
   def set_customer
     @customer = Customer.find_by(id: params[:customer_id])
+
     if @customer.nil?
-      render json: { errors: { customer_id: ["Error: couldn't find a customer with customer_id #{params[:customer_id]}"] } }, status: :not_found
+      render json: { 
+        errors: { 
+          customer_id: ["Error: couldn't find a customer with customer_id #{params[:customer_id]}"]
+        }
+      }, status: :not_found
     end
   end
 
   def set_video
     @video = Video.find_by(title: params[:title])
+
     if @video.nil?
-      render json: { errors: { title: ["Error: couldn't find a video with the title #{params[:title]}"] } }, status: :not_found
+      render json: { 
+        errors: { 
+          title: ["Error: couldn't find a video with title #{params[:title]}"]
+        }
+      }, status: :not_found
     end
   end
 
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  # TODO FILL IN THESE TESTS
+  describe "set_customer" do
+    it "correctly finds an existing valid customer" do
+      customer = customers(:shelley)
+
+      expect(self.set_customer).must_equal customer
+
+    end
+
+    it "returns json with an error message if the customer could not be found" do
+    end
+  end
+
+  describe "set_video" do
+    it "correctly finds and existing valid video" do
+    end
+
+    it "returns json with an error message if the video could not be found" do
+    end
+  end
 end
