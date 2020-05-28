a674657d52c77f5e2736a64523a02b89f804f621
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-27 17:04:56 -0700

    Added rentals controller, implemented controller filters for setting a customer and video before checkout and checkin methods.

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,28 @@
+class RentalsController < ApplicationController
+
+  # need private controller filters to set video_id and set customer_id before running checkout/in methods
+  before_action :set_customer, only: [:check_out, :check_in]
+  before_action :set_video, only: [:check_out, :check_in]
+
+  # check-out instance method
+
+
+  # check-in instance method
+
+  private
+
+  def set_customer
+    @customer = Customer.find_by(id: params[:customer_id])
+    if @customer.nil?
+      render json: { errors: { customer_id: ["Error: couldn't find a customer with customer_id #{params[:customer_id]}"] } }, status: :not_found
+    end
+  end
+
+  def set_video
+    @video = Video.find_by(title: params[:title])
+    if @video.nil?
+      render json: { errors: { title: ["Error: couldn't find a video with the title #{params[:title]}"] } }, status: :not_found
+    end
+  end
+
+end
