485b0daf740334a5f6ed9da67f82ed1be7ca22f4
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-28 19:58:25 -0700

    Started working on checkin tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,48 +6,73 @@ class RentalsController < ApplicationController
 
   # check-out instance method
 
-
-
-
-
-
-
-
-
-
-  
+#   def checkin
+#     new_video = Video.find_by(id: params[:video_id])
+#     new_customer = Customer.find_by(id: params[:customer_id])
+#     checkin = Rental.find_by(customer: new_customer, video: new_video)
+# ​
+#     if checkin.nil?
+#       render json: {
+#           errors: ['Not Found']
+#         }, status: :not_found
+#         return
+#     else
+#       render json: {
+#         customer_id: checkin.customer_id,
+#         video_id: checkin.video_id,
+#         videos_checked_out_count: checkin.decrement_videos_checked_out_count,
+#         available_inventory: checkin.increment_available_inventory
+#     }, 
+#       status: :ok
+#       return 
+#   end
 
   # check-in instance method
 
   def checkin
     # find the right rental instance in db to check in (video_id, customer_id)
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
     rental = Rental.find_by(params[:customer_id], params[:video_id])
     
     if rental
-      # decrease customer's videos_checked_out_count by 1
-      rental.customer.videos_checked_out_count -= 1
-      # increase movie's available_inventory count by 1
-      rental.video.available_inventory += 1
-      # update rental instance in db/save => returned = true
-      rental.returned = true
-
-      if rental.save
-        # might need to change this based on smoke tests
-        render json: {}, status: :ok
-      else
-        render json: { errors: rental.errors.messages }, status: :bad_request
-      end
-    else
-      # BAD REQUEST - change based on smoke tests?
+      # RENDER THE CORRECT JSON, update returned value and save
       render json: {
-        errors: {
-          rental: ["Customer #{@customer.id} has not checked out #{@video.title}"]
-        }
-      }, status: :not_found
+        customer_id: rental.customer_id,
+        video_id: rental.video_id,
+        videos_checked_out_count: rental.decrement_videos_checked_out_count,
+        available_inventory: rental.increment_available_inventory
+      }, status: :ok
+      # rental.returned = true
+      # rental.save
       return
-      end
+    else
+      # customer or video were nil
+      render json: {
+        errors: ['Not Found']
+        }, status: :not_found
+      return
     end
   end
+
+
+  #     if rental.save
+  #       # might need to change this based on smoke tests
+  #       render json: {}, status: :ok
+  #     else
+  #       render json: { errors: rental.errors.messages }, status: :bad_request
+  #     end
+  #   else
+  #     # BAD REQUEST - change based on smoke tests?
+  #     render json: {
+  #       errors: {
+  #         rental: ["Customer #{@customer.id} has not checked out #{@video.title}"]
+  #       }
+  #     }, status: :not_found
+  #     return
+  #     end
+  #   end
+  # end
 
   private
 
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -3,7 +3,23 @@ require "test_helper"
 describe RentalsController do
 
   describe "checkin" do
-    it "finds the correct customer and video ids for checking in an existing valid rental" do
+
+    before do
+      @customer = customers(:shelley)
+      @video = videos(:video_2)
+      @rental = Rental.new(customer_id: @customer.id, video_id: @video.id)
+      @rental_params = {
+        customer_id: @customer.id,
+        video_id: @video.id
+      }
+    end
+
+    it "finds the correct customer_id and video_id for checking in an existing valid rental" do
+      expect(@rental).wont_be_nil
+
+      expect{ post checkin_path, @rental_params }.wont_change "Rental.count"
+
+
     end
 
     it "returns 200 ok status and json when checking in an existing valid rental" do
@@ -15,10 +31,11 @@ describe RentalsController do
     it "increases the video's available count by 1 for a valid rental" do
     end
 
-    it "sets the rental's returned status to be true for a valid rental" do
-    end
+    # it "sets the rental's returned status to be true for a valid rental" do
+    # end
 
     it "returns error message and 404 not_found for a rental with a customer that does not exist" do
+
     end
 
     it "returns error message and 404 not_found for a rental with a video that does not exist" do
