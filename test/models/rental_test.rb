e440bd517b296a311a4d98e87a013a696674b8ba
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-05-28 21:01:39 -0700

    validation for the model tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do 
+    @rental = rentals(:rental_1)
+    @customer = customers(:shelley)
+    @video = videos(:video_1)
+  end 
+  describe "validations" do 
+    it "can not create rental without due_date" do 
+      @rental.due_date = nil 
+      expect(@rental.valid?).must_equal false 
+      expect(@rental.errors.messages[:due_date]).must_equal ["can't be blank"]
+    end 
+  end 
+  describe "relations" do 
+    it "will have customer id" do 
+      new_rental = Rental.new(customer_id: @customer.id, video_id: @video.id)
+      new_rental.save
+      expect(new_rental.customer_id).must_equal @customer.id
+    end 
+  end 
+  describe "increment_videos_checked_out_count" do 
+
+  end 
+  describe "decrement_available_inventory" do 
+    
+  end 
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,56 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do 
+    @video = videos(:video_1)
+    @customer = customers(:shelley)
+      
+  end 
+  describe "validations" do 
+    it "can not create video without title" do 
+      @video.title = nil 
+      @video.save
+      expect(@video.valid?).must_equal false 
+      expect(@video.errors.messages[:title]).must_equal ["can't be blank"]
+    end 
+    it "can not create video without title" do 
+      @video.title = nil 
+      @video.save
+      expect(@video.valid?).must_equal false 
+      expect(@video.errors.messages[:title]).must_equal ["can't be blank"]
+    end 
+    it "can not create video without overview" do 
+      @video.overview = nil 
+      @video.save
+      expect(@video.valid?).must_equal false 
+      expect(@video.errors.messages[:overview]).must_equal ["can't be blank"]
+    end 
+    it "can not create video without release_date" do 
+       @video.release_date = nil 
+       @video.save
+       expect(@video.valid?).must_equal false 
+       expect(@video.errors.messages[:release_date]).must_equal ["can't be blank"]
+     end 
+    it "can not create video without total_inventory" do 
+       @video.total_inventory = nil 
+       @video.save
+       expect(@video.valid?).must_equal false 
+       expect(@video.errors.messages[:total_inventory]).must_equal ["can't be blank"]
+     end 
+    it "can not create video without available_inventory" do 
+      @video.available_inventory = nil 
+      @video.save
+      expect(@video.valid?).must_equal false 
+      expect(@video.errors.messages[:available_inventory]).must_equal ["can't be blank"]
+    end 
+  end 
+  describe "relations" do 
+    it "can have rentals" do 
+      
+      rental_one = Rental.create!(customer_id: @customer.id, video_id: @video.id, due_date: Date.today + 7)
+      rental_two = Rental.create!(customer_id: @customer.id, video_id: @video.id, due_date: Date.today + 7)
+      @video.save
+      expect(@video.rentals.size).must_equal 2
+    end 
+  end 
 end
